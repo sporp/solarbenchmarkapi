2c78d76b7f5493a2de0d2454b41ce0b01f6815c5
sporp <greekpotato@gmail.com>
2024-03-26 18:27:18 -0400

    insert data and retrieve data for dashboard call

--- src/app.ts
+++ src/app.ts
@@ -25,14 +25,22 @@ import cors from 'cors';
 let fs = require('fs');
 dotenv.config({path: '.env'});
 
+import graphQLPowerCall from '../src/graphQLPowerCall.json'; 
+
+// let body = require('graphQLPowerCall.json');
+
+import { axiosClient } from './config/axiosConfig';
 import { knex } from './config/knexConfig';
 
 import mongoose, { createConnection, now } from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3456;
+
 const JWT_SECRET = process.env.JWT_SECRET || 'secrets_secrets_are_no_fun';
 const MONGO_STRING = process.env.MONGO_STRING || '';
+
+// import powerCall from './const/graphQLPowerCall.json' assert {type:'json'}
 
 app.use(cors({
     allowedHeaders: '*',
@@ -53,11 +61,12 @@ const userSchema = new mongoose.Schema({
 });
 
 const powerSchema = new mongoose.Schema({
-    username: String,
-    production: Array,
-    consumption: Array,
-    storage: Array,
-    timestamp: String
+    user_id: Number,
+    timestamp: Date,
+    production: Number,
+    consumption: Number,
+    storage: Number,
+    grid: Number
 });
 
 const User = mongoose.model("User", userSchema);
@@ -166,20 +175,153 @@ app.post('/upload', async (req, res) => {
     res.status(200).send('Data saved from webhook');
 });
 
-app.get('/dashboard', async (req, res) => {
+import axios from 'axios';
+// import { API_URL } from './envVars'
+// import getDilfCookie from './../Cookies';
 
+const baseURL = process.env.SOLAR_URL || '';
+
+/** i just did this so i didn't have to write the header every time */
+function getHeader(){
+    const config = {
+        headers: {
+            'Content-Type': 'application/json', //x-www-form-urlencoded application/json
+            'Authorization': `Bearer ${process.env.SOLAR_TOKEN || ''}`,
+            'timeout': 100000
+        }
+    }
+    return config;
+}
+
+// uploadGivenTimestamp
+// given timestamp requested in url, scrape data for that day and upload into db
+app.post('/upload/:date', async (req, res) => {
+    const date = req.params.date;
+    const stringItUp = true;
+
+    let response: any;
+    const config = getHeader();
+
+    try {
+        response = await axios.post(baseURL + 'graphql', graphQLPowerCall, config);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    // console.log(response);
+
+    if(!response || response.length < 1){
+        return
+    }
+
+    const powerData = response.data.data.power;
+
+    const production = powerData.powerDataSeries.production;
+    const consumption = powerData.powerDataSeries.consumption;
+    const storage = powerData.powerDataSeries.storage;
+    const grid = powerData.powerDataSeries.grid;
+
+    function logSeriesData () {
+        console.log(production);console.log(production.length + '\n');
+        console.log(consumption);console.log(consumption.length + '\n');
+        console.log(storage);console.log(storage.length + '\n');
+        console.log(grid);console.log(grid.length + '\n');
+    } 
+    // logSeriesData ();
+
+    const resultSolarData = [];
+
+    // powervalue is a timestamp + either consumption or generation
+    while(production.length && consumption.length && storage.length && grid.length){
+        const emptyArr = [null, null, null];
+
+        const p = production.shift();
+        const c = p[0] === consumption[0][0] ? consumption.shift() : emptyArr;
+        const s = p[0] === storage[0][0] ? storage.shift() : emptyArr;
+        const g = p[0] === grid[0][0] ? grid.shift() : emptyArr;
+
+        const powerRow = {
+            user_id: 1,
+            timestamp: new Date(p[0]),
+            production: parseFloat(p[1]),
+            consumption: parseFloat(c[1]),
+            storage: parseFloat(s[1]),
+            grid: parseFloat(g[1])
+        }
+
+        // rectify timezones here if any gaps exist, discard any timestamps that are less than current p[0]
+
+        resultSolarData.push(powerRow);
+    }
+
+    // console.log(resultSolarData);
+
+    await Power.insertMany(resultSolarData);
+
+    // Power.insertMany(resultSolarData).then(result => {
+    //     console.log(result)
+    // })
+
+    // const solarData = axios.get(url, {Bearer $})
+    // const { production, consumption, storage } = req.body;
+
+    /*
+    const user_id = 1;
+    const power = new Power({ user_id, userame: 'admin', production, consumption, storage, timestamp });
+    try {
+        await power.save();
+    } catch (e){
+        console.log(e)
+        res.sendStatus(401);
+    }
+    */
+    console.log('wrote to db. allegedly')
+    res.status(200).send('Data retrieved and sent to db');
+});
+
+// create 'insertMany' (if in mongoose) from JSON of downloaded data
+// OR python call can do a bunch of calls for upload 
+app.post('/uploadBulk', async (req, res) => {
+    const { production, consumption, storage, timestamp } = req.body;
+    const user_id = 1;
+    const power = new Power({ user_id, userame: 'admin', production, consumption, storage, timestamp });
+    try {
+        await power.save();
+    } catch (e){
+        console.log(e)
+        res.sendStatus(401);
+    }
+    res.status(200).send('Data saved from webhook');
+});
+
+app.get('/dashboard', async (req, res) => {
     const { api_key } = req.query;
-    console.log("api_key");
-    console.log(api_key);
+    // console.log("api_key");console.log(api_key);
 
     const Key = mongoose.model('key', keySchema);
     const key = await Key.find({ 'apiKey': api_key });
-    console.log("key")
-    console.log(key);
+    // console.log("key");console.log(key);
 
     if( !key || key.length < 1 ){
-        res.status(401).send('You aint got the votes');
+        res.status(401).send('The numbers mason, what do they mean?!');
         return;
+    }
+
+    const dashData = await Power.find();
+
+    // console.log(dashData);
+
+    let sum_production = 0;
+    let sum_consumption = 0;
+    let sum_storage = 0;
+    let sum_grid = 0;
+
+    for(const d of dashData){
+        if(d && d.production) sum_production += d.production;
+        if(d && d.consumption) sum_consumption += d.consumption;
+        if(d && d.storage) sum_storage += d.storage;
+        if(d && d.grid) sum_grid += d.grid;
     }
 
     // get data dump for dad
@@ -188,8 +330,10 @@ app.get('/dashboard', async (req, res) => {
 
     // worker to scrape data at some point, 1 to multiple times per day
     // download from sun service to grab data to ram to upload from service
+    // one single upload and one bulk upload route
 
     // dashboard html (xml?) element to display data on forum main page via api key
+    // make call to backend for dashboard, fetch and display data
 
 
 
@@ -207,9 +351,38 @@ app.get('/dashboard', async (req, res) => {
     // get sum of all power generated and spent from TS of site creation (sum of all entries)
 
     // list of TOP 5 USERS ('just most recent for MVP') for both POWER GENERATION AND USE
-    res.status(200).send('Key found');
+    res.status(200).json({
+        sum_production,
+        sum_consumption,
+        sum_storage,
+        sum_grid
+    });
 });
 
 app.listen(port, () => {
     console.log('Listening to you closely on port: ' + port);
 });
+
+
+// YYYY-MM-DD format. Ex: 2024-02-18
+function powerCallBuilder(date: string){
+    // return `[{"operationName": "FetchPowerData","variables": {"siteKey": "E_63596","interval": "five_minute","end": "${date}23:59:59","start": "${date}T00:00:00"},"query": "query FetchPowerData($interval: String!, $end: String!, $start: String!, $siteKey: String!) {\n  power(interval: $interval, end: $end, start: $start, siteKey: $siteKey) {\n    powerDataSeries {\n      production\n      consumption\n      storage\n      grid\n      __typename\n    }\n    __typename\n  }\n}\n"}]`
+    return [
+        {
+          operationName: "FetchPowerData",
+          variables: {
+            siteKey: "E_63596",
+            interval: "five_minute",
+            end: date + "23:59:59",
+            start: date + "T00:00:00"
+          },
+          query: "query FetchPowerData($interval: String!, $end: String!, $start: String!, $siteKey: String!) {\n  power(interval: $interval, end: $end, start: $start, siteKey: $siteKey) {\n    powerDataSeries {\n      production\n      consumption\n      storage\n      grid\n      __typename\n    }\n    __typename\n  }\n}\n"
+        }
+      ]
+}
+
+// let powerCall = powerCallBuilder(date);
+
+// if(stringItUp){
+//     JSON.stringify( powerCall );
+// }
---
+++ src/config/axiosConfig.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+// import { API_URL } from './envVars'
+// import getDilfCookie from './../Cookies';
+
+const baseURL = process.env.SOLAR_URL || '';
+
+export const axiosClient = axios.create({
+    baseURL: baseURL
+});
+
+/** i just did this so i didn't have to write the header every time */
+function getHeader(){
+    const config = {
+        headers: {
+            'Content-Type': 'application/json', //x-www-form-urlencoded application/json
+            'Authorization': `Bearer ${process.env.SOLAR_TOKEN || ''}`
+        }
+    }
+
+    return config;
+}
+
+export async function get (url: string) {
+    const config = getHeader();
+    try {
+        let response = await axios.get(url, config);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getBlob (url: string) {
+    const config = getHeader();
+    try {
+        let response = await axios.get(url, config);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function post (url: string, body: any) {
+    const config = getHeader();
+    try {
+        let response = await axios.post(url, body, config);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
